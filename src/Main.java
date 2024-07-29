2ab971266b849df780cb80d47de18b3e6f6af049
Engincan Gündüz <enqinn_64@hotmail.com>
2024-07-29 22:48:01 +0300

    Apply bugfix for r2.5.4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
+
 public class Main {
 	public static void main(String args[]) {
-		System.out.println("r2.5.4 Project");
+		System.out.println("Fixed Error on Project. New Version of Project is r2.5.4.1");
 	}
 }
