f00f40a0da43d6bdb86e8e2057fc4958023d965a
Engincan Gündüz <enqinn_64@hotmail.com>
2024-07-30 13:17:28 +0300

    Version r3.0

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 
 public class Main {
 	public static void main(String args[]) {
-		System.out.println("Fixed Error on Project. New Version of Project is r2.5.4.1");
+		System.out.println("New Version of Project is r3.0");
 	}
 }
